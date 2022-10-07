711fe1d071e3d1bbae5dad8ef7c54e4868b2f959
Jamshidbek <abdumannonovjamshidbek@gmail.com>
2022-10-07 20:30:44 +0500

    add new userservice

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"net"
+
 	"github.com/Jamshidbek1608/temp-service/config"
+	pb "github.com/Jamshidbek1608/temp-service/genproto"
 	"github.com/Jamshidbek1608/temp-service/pkg/db"
 	"github.com/Jamshidbek1608/temp-service/pkg/logger"
 	"github.com/Jamshidbek1608/temp-service/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -23,6 +28,21 @@ func main() {
 		log.Fatal("sqlx connection to postgres error", logger.Error(err))
 	}
 
-	userService := service.NewUserService
+	userService := service.NewUserService(connDB, log)
+
+	lis, err := net.Listen("tcp", cfg.RPCPort)
+	if err != nil {
+		log.Fatal("Error while listening: %v", logger.Error(err))
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	pb.RegisterUserServiceServer(s, userService)
+	log.Info("main: server running",
+		logger.String("port", cfg.RPCPort))
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Error while listening: %v", logger.Error(err))
+	}
 
 }
