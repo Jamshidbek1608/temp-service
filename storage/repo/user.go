7f187de520ae09f1d407d53004da3eab853cf94b
Jamshidbek <abdumannonovjamshidbek@gmail.com>
2022-10-07 19:28:19 +0500

    add new func

--- storage/repo/user.go
+++ storage/repo/user.go
@@ -1,19 +1,10 @@
 package repo
+
 import (
-	"github.com/jmoiron/sqlx"
 	pb "github.com/Jamshidbek1608/temp-service/genproto"
 )
 
-type userRepo struct{
-	db *sqlx.DB
+// UserStorageI ...
+type UserStorageI interface {
+	Create(*pb.User) (*pb.User, error)
 }
-
-func NewUserRepo(db *sqlx.DB) *userRepo {
-	return &userRepo{db: db}
-}
-
-func (r *userRepo) Create(user *pb.User) (*pb.User, error) {
-	userRepo := pb.User{}
-	err:=r.
-}
-
---
+++ storage/storage.go
@@ -0,0 +1,26 @@
+package storage
+
+import (
+	"github.com/Jamshidbek1608/temp-service/storage/repo"
+	"github.com/jmoiron/sqlx"
+)
+
+type IStorage interface {
+	User() repo.UserStorageI
+}
+
+type storagePg struct {
+	db       *sqlx.DB
+	userRepo repo.UserStorageI
+}
+
+func NewStoragePg(db *sqlx.DB) *storagePg {
+	return &storagePg{
+		db:       db,
+		userRepo: postgres.NewUserRepo(db),
+	}
+}
+
+func (s storagePg) User() repo.UserStorageI {
+	return s.userRepo
+}
